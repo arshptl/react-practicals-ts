9d0583c5c3e80454efe892f9254606bddf20409a
harshptl14 <harshptl714@gmail.com>
2022-05-25 10:45:39 +0530

    converted UserPage into classbased | need to fix

--- practical-05-06-07/src/pages/UsersPage.js
+++ practical-05-06-07/src/pages/UsersPage.js
@@ -1,110 +1,172 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components';
-import Header from '../components/users/Header';
-import HoverCard from '../components/users/HoverCard';
-import UserList from '../components/users/UserList';
+import { render } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Header from "../components/users/Header";
+import HoverCard from "../components/users/HoverCard";
+import UserList from "../components/users/UserList";
 
 const StyledDiv = styled.div`
-width: 100%;
-margin: 0 auto;
-background-color: #FFFFFF;
-padding: 2em;
-min-width: 24em;
-max-width: 60em;
+  width: 100%;
+  margin: 0 auto;
+  background-color: #ffffff;
+  padding: 2em;
+  min-width: 24em;
+  max-width: 60em;
 
-.styledError{
-padding: 0.7em;
-margin-top: 0.5em;
-color: red;
-background-color: #ffe6e6;
-}
+  .styledError {
+    padding: 0.7em;
+    margin-top: 0.5em;
+    color: red;
+    background-color: #ffe6e6;
+  }
 
-.styledEmptyList{
+  .styledEmptyList {
     padding: 0.7em;
     margin-top: 0.5em;
     text-align: center;
     background-color: #e9fbf3;
     color: #24c27b;
-}
+  }
 
-@media(min-width: 37.5em) {
+  @media (min-width: 37.5em) {
     width: 100%;
     padding: 2em;
-}
+  }
 
-@media(min-width: 62.5em) {
+  @media (min-width: 62.5em) {
     width: 100%;
     padding: 3.25em;
-}
+  }
 `;
 
 const StyledButtonDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
 `;
 
 const StyledButton = styled.button`
+  padding: 0.6em 0.9em;
+  background-color: ${(props) =>
+    props.active === props.buttonId ? "#F5B23E" : "#ffeac4"};
+  border: 0;
+  cursor: pointer;
 
-        padding: 0.6em 0.9em;
-        background-color: ${props => props.active === props.buttonId ? "#F5B23E" : "#ffeac4"};
-        border: 0;
-        cursor: pointer;
-
-        :hover{
-            background-color: #F5B23E;
-            outline: 0.3em solid #ffeac4;
-        }
+  :hover {
+    background-color: #f5b23e;
+    outline: 0.3em solid #ffeac4;
+  }
 `;
 
-const UsersPage = () => {
-    const [showPopup, setShowPopup] = useState(false);
-    const [selectedUser, setSelectedUser] = useState();
-    const [users, setUsers] = useState([]);
-    const [active, setActive] = useState(1);
-    const apis = [{ link: process.env.REACT_APP_API_ENDPOINT_FIRST, id: 1 }, { link: process.env.REACT_APP_API_ENDPOINT_SECOND, id: 2 }];
+const getUserData = async (
+  apiLink = process.env.REACT_APP_API_ENDPOINT_FIRST,
+  id = 1
+) => {
+  const res = await fetch(apiLink);
+  const json = await res.json();
+  console.log(json);
+  //   setUsers(json.data);
+  //   setActive(id);
+  await this.setState(() => {
+    // ...this.state,
+    // users: json.data,
+    // active: id,
+    return {
+      users: json.data,
+      active: id,
+    };
+  });
+  return json.data;
+};
+class UsersPage extends React.Component {
+  state = {
+    showPopup: false,
+    selectedUser: {},
+    users: [],
+    active: 1,
+  };
 
-    const getUserData = async (apiLink = process.env.REACT_APP_API_ENDPOINT_FIRST, id = 1) => {
-        const res = await fetch(apiLink);
-        const json = await res.json();
-        setUsers(json.data);
-        setActive(id);
-        return json.data;
-    }
+  componentDidMount() {
+    getUserData();
+  }
+  render() {
+    // const [showPopup, setShowPopup] = useState(false);
+    // const [selectedUser, setSelectedUser] = useState();
+    // const [users, setUsers] = useState([]);
+    // const [active, setActive] = useState(1);
+    const { showPopup, selectedUser, users, active } = this.state;
+    const apis = [
+      { link: process.env.REACT_APP_API_ENDPOINT_FIRST, id: 1 },
+      { link: process.env.REACT_APP_API_ENDPOINT_SECOND, id: 2 },
+    ];
 
-    useEffect(() => {
-        getUserData();
-    }, []);
+    // useEffect(() => {
+    //   getUserData();
+    // }, []);
 
-    const closePopup = React.useCallback(() => {
-        setShowPopup(false);
-    }, []);
-    
-    const showPopupHandler = React.useCallback((item) => {
-        setShowPopup(true);
-        setSelectedUser(item);
-    }, []);
+    const closePopup = () => {
+      // setShowPopup(false);
+      this.setState({
+        ...this.state,
+        showPopup: false,
+      });
+    };
+
+    const showPopupHandler = (item) => {
+      //   setShowPopup(true);
+      //     setSelectedUser(item);
+      this.setState({
+        ...this.state,
+        setShowPopup: true,
+        setSelectedUser: item,
+      });
+    };
 
     const handleButtonClick = (apiLink, id) => {
-        getUserData(apiLink, id);
-    }
-
-
+      getUserData(apiLink, id);
+    };
     return (
-        <StyledDiv>
-            {users.length === null ? <div>There is a problem in API</div> : <><Header />
+      <StyledDiv>
+        {users.length === null ? (
+          <div>There is a problem in API</div>
+        ) : (
+          <>
+            <Header />
             {users.map((obj) => {
-                return <UserList userData={obj} showPopupHandler={showPopupHandler}
-                    closePopup={closePopup} key={obj.id} />;
+              return (
+                <UserList
+                  userData={obj}
+                  showPopupHandler={showPopupHandler}
+                  closePopup={closePopup}
+                  key={obj.id}
+                />
+              );
             })}
             {showPopup && <HoverCard selectedUser={selectedUser} />}
             <StyledButtonDiv>
-                <StyledButton className='StyledButtons' active={active} buttonId={1} onClick={() => handleButtonClick(apis[0].link, 1)}>1</StyledButton>
-                <StyledButton className='StyledButtons' active={active} buttonId={2} onClick={() => handleButtonClick(apis[1].link, 2)}>2</StyledButton>
-            </StyledButtonDiv></>}
-        </StyledDiv>
-    )
+              <StyledButton
+                className="StyledButtons"
+                active={active}
+                buttonId={1}
+                onClick={() => handleButtonClick(apis[0].link, 1)}
+              >
+                1
+              </StyledButton>
+              <StyledButton
+                className="StyledButtons"
+                active={active}
+                buttonId={2}
+                onClick={() => handleButtonClick(apis[1].link, 2)}
+              >
+                2
+              </StyledButton>
+            </StyledButtonDiv>
+          </>
+        )}
+      </StyledDiv>
+    );
+  }
 }
 
-export default UsersPage
+export default UsersPage;
